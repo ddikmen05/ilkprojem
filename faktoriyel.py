7d21db1df11e497846869968fc9c803c73d51224
ddikmen05 <ddikmen311@gmail.com>
2022-10-31 18:57:31 +0300

    faktoriyel4

---
+++ faktoriyel.py
@@ -0,0 +1,8 @@
+a=int(input("bir sayı söyle"))
+
+sonuc=1
+
+for i in range(a):
+    sonuc=sonuc*(i+1)
+
+print(sonuc)
---
+++ usalma.py
@@ -0,0 +1,9 @@
+a=int(input("bir sayı söyle"))
+b=int(input("bir sayı daha söyle"))
+
+sonuc=1
+
+for i in range(b):
+    sonuc=sonuc*a
+
+print("a üzeri b şudur",sonuc)
